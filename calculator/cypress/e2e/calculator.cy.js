55775defa0ad0ce379b4ccc3543a244f9b88b48e
ungHeung <t01047377477@gmail.com>
2023-07-13 20:57:21 +0900

    Fix : 오타 수정

--- calculator/cypress/e2e/calculator.cy.js
+++ calculator/cypress/e2e/calculator.cy.js
@@ -8,13 +8,20 @@ describe("계산기 앱 테스트", () => {
     });
 
     it("숫자 버튼을 클릭하면 숫자가 화면에 표시된다.", () => {
-        cy.get(".digit").contains(1).click;
+        cy.get(".digit").contains(1).click();
         cy.get("#total").should("have.text", 1);
     });
 
     it("2개의 숫자 버튼 클릭시 두개의 숫자가 화면에 표시된다.", () => {
-        cy.get(".digit").contains(1).click;
-        cy.get(".digit").contains(2).click;
+        cy.get(".digit").contains(1).click();
+        cy.get(".digit").contains(2).click();
         cy.get("#total").should("have.text", 12);
+    });
+
+    it("3개의 숫자 버튼 클릭시 두개의 숫자가 화면에 표시된다.", () => {
+        cy.get(".digit").contains(1).click();
+        cy.get(".digit").contains(2).click();
+        cy.get(".digit").contains(3).click();
+        cy.get("#total").should("have.text", 123);
     });
 });
