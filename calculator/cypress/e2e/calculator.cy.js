1147067061b4b55cf8dcb6d2ad38adfccadacc70
ungHeung <t01047377477@gmail.com>
2023-07-13 21:04:31 +0900

    Refactor : 숫자 버튼 클릭 기능을 함수처리하여 중복 코드 줄이도록 리팩

--- calculator/cypress/e2e/calculator.cy.js
+++ calculator/cypress/e2e/calculator.cy.js
@@ -1,3 +1,13 @@
+const clickDigitButtons = (digits = []) => {
+    digits.forEach((digit) => {
+        cy.get(".digit").contains(digit).click();
+    });
+};
+
+const checkDisplayValue = (value) => {
+    cy.get("#total").should("have.text", value);
+};
+
 describe("계산기 앱 테스트", () => {
     beforeEach("페이지 방문", () => {
         cy.visit("../../index.html");
@@ -8,20 +18,22 @@ describe("계산기 앱 테스트", () => {
     });
 
     it("숫자 버튼을 클릭하면 숫자가 화면에 표시된다.", () => {
-        cy.get(".digit").contains(1).click();
-        cy.get("#total").should("have.text", 1);
+        clickDigitButtons([1]);
+        checkDisplayValue(1);
     });
 
     it("2개의 숫자 버튼 클릭시 두개의 숫자가 화면에 표시된다.", () => {
-        cy.get(".digit").contains(1).click();
-        cy.get(".digit").contains(2).click();
-        cy.get("#total").should("have.text", 12);
+        clickDigitButtons([1, 2]);
+        checkDisplayValue(12);
     });
 
     it("3개의 숫자 버튼 클릭시 두개의 숫자가 화면에 표시된다.", () => {
-        cy.get(".digit").contains(1).click();
-        cy.get(".digit").contains(2).click();
-        cy.get(".digit").contains(3).click();
-        cy.get("#total").should("have.text", 123);
+        clickDigitButtons([1, 2, 3]);
+        checkDisplayValue(123);
+    });
+
+    it("0이 맨 앞에 오는 경우 표시하지 않는다.", () => {
+        clickDigitButtons([0, 2, 3]);
+        checkDisplayValue(23);
     });
 });
