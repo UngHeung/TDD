d5f5f89cbc539354c5fa70ff6634a4559b7b7c03
ungHeung <t01047377477@gmail.com>
2023-07-13 20:00:48 +0900

    Feat : 페이지 접속 테스트

--- calculator/cypress/e2e/calculator.cy.js
+++ calculator/cypress/e2e/calculator.cy.js
@@ -1,5 +1,5 @@
-describe("template spec", () => {
-    it("passes", () => {
-        cy.visit("https://example.cypress.io");
+describe("계산기 앱 테스트", () => {
+    it("계산기 페이지 접속", () => {
+        cy.visit("../../index.html");
     });
 });
